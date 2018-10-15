586b4d61a0c90693efa8d3b451c1d3b8bbd2dbf3
Christian Broberg <cb@webhouse.dk>
2018-10-16 00:48:53 +0200

    Test initial setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-#chmod +x setup.sh
-# wget https://raw.githubusercontent.com/senti-platform/senti-mqtt-client/master/setup.sh && bash setup.sh
+# wget -O - https://github.com/senti-platform/senti-client-setup/blob/master/setup.sh | bash
 
-# Log in and sudo -i
-
-#- Install all needed helper software
+#- Check if needed software exist
 #- Create paths
 #- Download assets (wget + unpack)
 #- Copy to destinations
@@ -13,27 +10,65 @@
 #- Add systemd entries
 #- Start systemd services
 
-
-
 clear
+
+#- Login
+sudo -i
 
 echo Setting up Senti-MQTT-Client
 
-sudo mkdir -p /srv/nodejs
+# Create temporary tools work directory
+sudo mkdir -p /usr/local/tools
+cd /usr/local/tools
+pwd
 
+#- Install all needed helper software
+#- Check if needed software exist
+echo
+echo Checking if needed software is installed ... 
+
+GIT=/usr/bin/git
+if [ ! -e "$GIT" ]; then
+    echo "GIT is not installed."
+	echo "Installing GIT"
+	sudo apt install git-all
+	git --version
+else 
+    echo "GIT is already installed"
+fi 
+
+NODE=/usr/local/bin/node
+if [ ! -e "$NODE" ]; then
+    echo "NODEJS is not installed."
+	echo "Installing NODEJS"
+	sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash && sudo apt-get install -y nodejs && sudo apt-get install build-essential
+	node -v
+	npm -v
+else 
+    echo "NODE is already installed"
+fi
+
+echo Installing MQTT
+sudo npm install mqtt -g
+
+#- Create paths
+sudo mkdir -p /srv/nodejs/senti
 
 echo 
-echo Pulling latest updates from GitHub
-cd /srv/nodejs/
+echo Fetching latest versions of Senti software from GitHub
+cd /srv/nodejs/senti
 sudo git -C senti-mqtt-client pull || git clone https://github.com/senti-platform/senti-mqtt-client.git
+sudo git -C senti-watchman pull || git clone https://github.com/senti-platform/senti-watchman.git
 echo 
 
 echo Installing modules and/or upgrades
 echo
 cd senti-mqtt-client
 sudo npm install
+sudo mkdir -p /srv/nodejs/senti/senti-mqtt-client/logs
+cd ../senti-watchman
+sudo npm install
 
-sudo mkdir -p /srv/nodejs/senti-mqtt-client/logs
 echo
 echo Starting senti-mqtt-client service ...
 echo
